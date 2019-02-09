518124f5116310a7578b603c26fa1bf28cefdb77
Farshid <Farshid@FARSHID-PC>
2019-02-09 22:33:29 +0330

    .

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,32 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            username = username.ToLower();
+
+            if (await _repo.UserExist(username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+
+
+
+        }
     }
 }
