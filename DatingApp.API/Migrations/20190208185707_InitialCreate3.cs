d165afc7cc75ee3312d545ca66d23d4a263f244a
Farshid <Farshid@FARSHID-PC>
2019-02-09 19:05:16 +0330

    Initial commit

---
+++ DatingApp.API/Migrations/20190208185707_InitialCreate3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class InitialCreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Family",
+                table: "Values",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Family",
+                table: "Values");
+        }
+    }
+}
