59385b0f8ca46f1f4276fd4c8a481e096bd7814a
Farshid <Farshid@FARSHID-PC>
2019-02-10 21:43:24 +0330

    End 02 Section

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="کلمه عبور بین 4 تا 8 کاراکتر باید باشد")]
+        public string  Password { get; set; }
+    }
+}
